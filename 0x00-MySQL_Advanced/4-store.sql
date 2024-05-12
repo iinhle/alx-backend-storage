13ca02d2f505b03c2313a393137e967971183276
iinhle <Sawcyzee@gmail.com>
2024-05-11 19:18:53 -0700

    BestBandEver

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,6 @@
---  SQL script that creates a trigger that decreases
--- the quantity of an item after adding a new order.
-
-CREATE TRIGGER decrement_quantity AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END;
+-- Task 4: Buy buy buy - creates a trigger that decreases the quantity
+-- of an item after adding a new order
+CREATE TRIGGER order_decrease BEFORE INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
