dfa5b83111dfbd5531808d21f8067a3602089ccf
iinhle <Sawcyzee@gmail.com>
2024-05-11 18:29:58 -0700

    done

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,21 @@
+-- a SQL script that creates a stored procedure AddBonus
+--  that adds a new correction for a student
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+    DECLARE project_id INT;
+    
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+    
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+    
+END//
+
+DELIMITER ;
